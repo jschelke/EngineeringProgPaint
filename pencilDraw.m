9c028c7162c67a7a79a5abfce98d01ae40499f82
unknown <pieterpartous@hotmail.com>
2017-12-16 20:44:43 +0100

    pencildraw

--- pencilDraw.m
+++ pencilDraw.m
@@ -2,35 +2,33 @@ function pixelsToDraw = pencilDraw (handles)
     pixelsToDraw = handles.Image;
 %     pixelsToDraw(ceil(handles.currentMousePos(1,2)), ceil(handles.currentMousePos(1,1)), :) = 0;
 
-    xDiff = handles.currentMousePos(1,2) - handles.previousMousePos(1,2)
-    yDiff = handles.currentMousePos(1,1) - handles.previousMousePos(1,1)
+    xDiff = round(handles.currentMousePos(1,1) - handles.previousMousePos(1,1));
+    yDiff = round(handles.currentMousePos(1,2) - handles.previousMousePos(1,2));
     
     if abs(xDiff) >= abs(yDiff)
         if xDiff >= 0
-            for x = handles.previousMousePos(1,2):handles.currentMousePos(1,2)
-                y = round(yDiff / xDiff * (handles.previousMousePos(1,2)) + handles.previousMousePos(1,2));
-
-                pixelsToDraw(round(x), round(y) , :) = 0;
+            for x = round(handles.previousMousePos(1,1)):round(handles.currentMousePos(1,1))
+                y = round(yDiff / xDiff * (x - handles.previousMousePos(1,1)) + handles.previousMousePos(1,2));  
+                pixelsToDraw(y, x, :) = 0;
             end
         else
-            for x = handles.previousMousePos(1,2):-1:handles.currentMousePos(1,2)
-                y = round(yDiff / xDiff * (handles.previousMousePos(1,2)) + handles.previousMousePos(1,2));
-
-                pixelsToDraw(round(x), round(y) , :) = 0;
+            for x = round(handles.previousMousePos(1,1)):-1:round(handles.currentMousePos(1,1))
+                y = round(yDiff / xDiff * (x - handles.previousMousePos(1,1)) + handles.previousMousePos(1,2));
+                pixelsToDraw(y, x, :) = 0;
             end
         end 
     else
         if yDiff >= 0
-            for y = handles.previousMousePos(1,1):handles.currentMousePos(1,1)
-                x = round(xDiff / yDiff * (handles.previousMousePos(1,1)) + handles.previousMousePos(1,1));
+            for y = round(handles.previousMousePos(1,2)):round(handles.currentMousePos(1,2))
+                x = round(xDiff / yDiff * (y - handles.previousMousePos(1,2)) + handles.previousMousePos(1,1));
 
-                pixelsToDraw(round(x), round(y) , :) = 0;
+                pixelsToDraw(y, x, :) = 0;
             end
         else
-            for y = handles.previousMousePos(1,1):-1:handles.currentMousePos(1,1)
-                x = round(xDiff / yDiff * (handles.previousMousePos(1,1)) + handles.previousMousePos(1,1));
+            for y = round(handles.previousMousePos(1,2)):-1:round(handles.currentMousePos(1,2))
+                x = round(xDiff / yDiff * (y - handles.previousMousePos(1,2)) + handles.previousMousePos(1,1));
 
-                pixelsToDraw(round(x), round(y) , :) = 0;
+                pixelsToDraw(y, x , :) = 0;
             end
         end
     end
