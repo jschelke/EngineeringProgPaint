4c8a564d36616b8982a19048772a9aa97d14f4cf
jschelke <schelkens.jeroen@gmail.com>
2017-12-16 17:35:36 +0100

    added rotation function

--- ButtonRotate_Callback.m
+++ ButtonRotate_Callback.m
@@ -1,6 +1,6 @@
 function ButtonRotate_Callback(hObject, eventdata, handles)
 
-    handles.Image = permute(handles.Image);
+    handles.Image = rot90(handles.Image);
     ImageSize = size(handles.Image);
     changeCanvasSize(handles,ImageSize(2),ImageSize(1));
 
