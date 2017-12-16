15bb7ab7d28f8e191104dc86fba1a01998a7879f
jschelke <schelkens.jeroen@gmail.com>
2017-12-16 19:51:41 +0100

    added rotate button

--- Main.m
+++ Main.m
@@ -22,7 +22,7 @@ function varargout = Main(varargin)
 
 % Edit the above text to modify the response to help Main
 
-% Last Modified by GUIDE v2.5 16-Dec-2017 18:53:18
+% Last Modified by GUIDE v2.5 16-Dec-2017 19:50:57
 
 % Begin initialization code - DO NOT EDIT
     gui_Singleton = 1;
@@ -57,7 +57,6 @@ function Main_OpeningFcn(hObject, eventdata, handles, varargin)
     handles.currentMousePos = [0 0];
     handles.previousMousePos = [0 0];
     handles.pencilSize = 1;
-    handles.brushShape = [1; 1];
     
     % ----------------------
     %      Mouse events
