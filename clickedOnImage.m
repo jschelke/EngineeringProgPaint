cced0566130575c50578b848dd49e54975fd911d
jschelke <schelkens.jeroen@gmail.com>
2017-12-16 12:55:58 +0100

    Testinf mouse detection

--- clickedOnImage.m
+++ clickedOnImage.m
@@ -1,6 +1,7 @@
-function [] = clickedOnImage(object,~)
+function [] = clickedOnImage(hObject,~,handles)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    
+    handles.output = hObject;
+    MousePosition = get(handles.Main_Axes,'CurrentPoint')
 end
 
