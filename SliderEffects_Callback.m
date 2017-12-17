00bd040edaae1b83d84964fef72010d108a402e5
jschelke <schelkens.jeroen@gmail.com>
2017-12-17 15:02:00 +0100

    Some GUI tweaking

--- SliderEffects_Callback.m
+++ SliderEffects_Callback.m
@@ -5,4 +5,9 @@ function SliderEffects_Callback(hObject, eventdata, handles)
 
     % Hints: get(hObject,'Value') returns position of slider
     %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+    handles.output = hObject;
+    set(handles.LabelEffects,'String', [num2str(round(get(handles.SliderEffects,'Value')*100)) '%']);
+    % Update handles structure
+    guidata(hObject, handles);
+    
 end
