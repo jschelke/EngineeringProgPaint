2979635ad37bf15d25056daec8f9e656233f8f6a
unknown <pieterpartous@hotmail.com>
2017-12-18 23:22:23 +0100

    autozoom

--- ButtonOpenFile_Callback.m
+++ ButtonOpenFile_Callback.m
@@ -3,12 +3,11 @@ function ButtonOpenFile_Callback(hObject, eventdata, handles)
 SupportedFormats = {'*.jpg;*.bmp;*.png*',...
  'Picture Formats (*.jpg,*.bmp,*.png)'}; %These formats are now supported
 
-%[filename, pathname] = uigetfile(SupportedFormats);
-pathname = [pwd, '\'];
+[filename, pathname] = uigetfile(SupportedFormats);
 
+%pathname = [pwd, '\'];
 %filename = 'test.png';
-
-filename = 'RedEyes2.jpg';
+%filename = 'RedEyes2.jpg';
 
 handles.Image = imread([pathname filename]);
 handles.Image = double(handles.Image) / 255;
@@ -19,8 +18,13 @@ changeCanvasSize(handles,ImageSize(2),ImageSize(1));
 handles.ImageShow = handles.Image;
 handles.ImagePlot = image(handles.Image);
 
+
+if (max(ImageSize(1), ImageSize(2)) <= handles.maxCanvasSize)
+    handles.zoomValue = 100;
+else
+    handles.zoomValue = round(100 * handles.maxCanvasSize / max(ImageSize(1), ImageSize(2)));
+end
 handles.imageAxesLimits = [1 ImageSize(2); 1 ImageSize(1)];
-handles.zoomValue = 100;
 set(handles.zoomTextInputValue, 'String', num2str(handles.zoomValue));
 set(handles.zoomSliderValue, 'Value', handles.zoomValue);
 setImageAxis(handles);
